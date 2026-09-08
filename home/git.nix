b53f3b47457fff4af5350a77e72269f1394b5bac
AngyySB <johe261@gmail.com>
2026-09-08 10:49:59 +0200

    current updated configs for laptop

--- home/git.nix
+++ home/git.nix
@@ -1,19 +1,20 @@
-{...}:
+{ ... }:
 {
-    programs.git = {
-        enable = true;
-      settings = {user = {
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
 
-
-	name = "AngyySB";
-	email = "johe261@gmail.com";
-      };};
-
-	includes = [
-	{
-	    condition = "gitdir:~/NTNU/";
-	    contents.user.email = "jonaher@stud.ntnu.no";
-	}
-	];
+        name = "AngyySB";
+        email = "johe261@gmail.com";
+      };
     };
+
+    includes = [
+      {
+        condition = "gitdir:~/NTNU/";
+        contents.user.email = "jonaher@stud.ntnu.no";
+      }
+    ];
+  };
 }
